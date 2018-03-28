90bc3262861fb439cbddd0c82fde8c4b4ff6f907
CD <CD@CDD.CO>
2018-03-28 19:53:08 -0400

    Custom diagrams...

--- Main/ProjectDiagramV1/MainWindow.xaml.cs
+++ Main/ProjectDiagramV1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MindFusion.Diagramming.Wpf;
+using MindFusion.Diagramming.Wpf.Layout;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,13 +26,66 @@ namespace ProjectDiagramV1
         {
             InitializeComponent();
 
-            shapeList.ItemsSource = MindFusion.Diagramming.Wpf.Shape.Shapes.Cast<MindFusion.Diagramming.Wpf.Shape>().Select(
-                shape => new ShapeNode { Shape = shape, Bounds = new Rect(0, 0, 40, 40) });
+            // load the built in nodes list
+            //shapeList.ItemsSource = MindFusion.Diagramming.Wpf.Shape.Shapes.Cast<MindFusion.Diagramming.Wpf.Shape>().Select(
+            //  shape => new ShapeNode { Shape = shape, Bounds = new Rect(0, 0, 40, 40) });
+
+            // Add a specific node to the list
+            shapeList.Items.Add(new ShapeNode { Shape = Shapes.DividedProcess, Bounds = new Rect(0, 0, 40, 40) });
+
+            // testing below
+            diagram.LinkHeadShape = ArrowHeads.Triangle;
+            diagram.DiagramLinkStyle = new System.Windows.Style();
+            diagram.DiagramLinkStyle.Setters.Add(new Setter(DiagramLink.BrushProperty, Brushes.Black));
+            diagram.LinkHeadShapeSize = 12;
+            diagram.LinkShape = LinkShape.Cascading;
+            diagram.RoundedLinks = true;
+            diagram.RoundedLinksRadius = 10;
+            diagram.LinkSegments = 3;
+            diagram.Behavior = Behavior.Modify;
         }
         private void diagram_Drop(object sender, DragEventArgs e)
         {
+           
             ShapeNode node = diagram.Items[diagram.Items.Count - 1] as ShapeNode;
             node.Bounds = new Rect(node.Bounds.Left, node.Bounds.Top, 75, 75);
+
+        }
+        private void OnWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            // Create the hierarchy
+            //var node1 = new ShapeNode { Shape = Shapes.DividedProcess, Bounds = new Rect(0, 0, 40, 40) };
+            //var node1 = new ContainerNode { Shape = SimpleShape.Rectangle, Bounds = new Rect(0, 0, 200, 200) };
+
+            var node1 = new CustomDatabaseDiag
+            {
+                Bounds = new Rect(0, 0, 300, 160),
+                ClassName = "Mike Powell",
+                MemberName = "Member 1",
+                //emberName = "Member 1",
+                Index = 0
+            };         
+            diagram.Nodes.Add(node1);
+
+            var node2 = new CustomDatabaseDiag
+            {
+                Bounds = new Rect(0, 0, 300, 160),
+                ClassName = "Emily Williams",
+                MemberName = "Emily is the leader highest in the PR hierarchy.",
+                Index = 1
+            };
+            diagram.Nodes.Add(node2);
+
+            TreeLayout layout = new TreeLayout();
+            layout.Type = TreeLayoutType.Centered;
+            layout.LinkStyle = TreeLayoutLinkType.Cascading3;
+            layout.Direction = TreeLayoutDirections.TopToBottom;
+            layout.KeepRootPosition = false;
+            layout.LevelDistance = 40;
+            layout.Arrange(diagram);
+
         }
     }
+
+
 }
