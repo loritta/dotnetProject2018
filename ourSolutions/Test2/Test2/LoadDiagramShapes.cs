4e9dee23c7466e2be691b2eaec4142a8790d03cf
larisaWindows <lara@mail.com>
2018-03-30 16:12:16 -0400

    New file for the solution
    +the diagram types

---
+++ ourSolutions/Test2/Test2/LoadDiagramShapes.cs
@@ -0,0 +1,20 @@
+﻿using MindFusion.Diagramming.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+namespace Test2
+{
+
+    class LoadDiagramShapes
+    {
+        public static void LoadUmlNodes(NodeListView shapeList)
+        {
+            // Add a specific node to the list           
+            shapeList.Items.Add(new ShapeNode { Shape = Shapes.DividedProcess, Bounds = new Rect(0, 0, 40, 40) });
+        }
+    }
+
+}
