b83238ecdf096a7c69a8bb65c1deea8d7191f2b5
STUDENT.JAC.Lara <lara@lara.com>
2018-04-05 11:24:58 -0400

    Global class

---
+++ ourSolutions/Test2/Test2/Globals.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MindFusion.Diagramming.Wpf;
+
+namespace Test2
+{
+    class Globals
+    {
+        public static Diagram diagram;
+        
+    }
+}
